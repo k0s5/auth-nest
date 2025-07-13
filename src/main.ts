fa53ad9a36c050fb75a33d31c24a84cec7019140
koss <jobs.koss@gmail.com>
2025-07-13 21:38:00 +0300

    feat: added cookieParser and configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { AppModule } from '@core/app'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const configService = app.get(ConfigService)
+  app.use(cookieParser(configService.getOrThrow<string>('COOKIE_SECRET')))
+  await app.listen(configService.getOrThrow<number>('PORT'))
 }
 bootstrap()
