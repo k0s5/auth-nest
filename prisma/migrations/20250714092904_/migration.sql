d755c10721fbf5764e6c0ea0fc1b96cc5eb3e914
koss <jobs.koss@gmail.com>
2025-07-14 19:57:48 +0300

    feat: added Session model and new migration

---
+++ prisma/migrations/20250714092904_/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+    "fingerprint" TEXT NOT NULL,
+    "ip" TEXT,
+    "ua" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "revokedAt" TIMESTAMP(3),
+    "userId" TEXT,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_accessToken_key" ON "sessions"("accessToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_refreshToken_key" ON "sessions"("refreshToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_fingerprint_key" ON "sessions"("fingerprint");
+
+-- CreateIndex
+CREATE INDEX "sessions_accessToken_idx" ON "sessions"("accessToken");
+
+-- CreateIndex
+CREATE INDEX "sessions_refreshToken_idx" ON "sessions"("refreshToken");
+
+-- CreateIndex
+CREATE INDEX "sessions_fingerprint_idx" ON "sessions"("fingerprint");
+
+-- CreateIndex
+CREATE INDEX "sessions_userId_idx" ON "sessions"("userId");
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
