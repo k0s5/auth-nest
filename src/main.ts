18ef8e652d3e590e85470ab1c3d5697ea51733f3
koss <jobs.koss@gmail.com>
2025-07-14 19:59:10 +0300

    fix: frixed cookie parser import

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { AppModule } from '@core/app'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api/auth')
   const configService = app.get(ConfigService)
+
   app.use(cookieParser(configService.getOrThrow<string>('COOKIE_SECRET')))
   await app.listen(configService.getOrThrow<number>('PORT'))
 }
